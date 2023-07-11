ff69e7a4090d1819f9dc6f9f7c7d306f314035aa
Jesse Carson <jmcarson222@gmail.com>
2023-07-11 14:41:18 -0500

    pages / error page structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -22,7 +23,9 @@ const Landing = () => {
             accusamus modi, fugit cum optio numquam nesciunt magnam? Modi,
             dolorem quisquam?
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
