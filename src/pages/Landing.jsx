5b8f8b338f40681e059867dcbb376f02550c4404
Jesse Carson <jmcarson222@gmail.com>
2023-07-11 11:33:16 -0500

    landing page structure and styling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,33 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
 
 const Landing = () => {
-  return <h4>Landing</h4>;
+  return (
+    <Wrapper>
+      {/* nav */}
+      <nav>
+        <Logo />
+      </nav>
+
+      {/* login/register */}
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            nam veritatis saepe, impedit et dicta officiis, esse dolor quis
+            accusamus modi, fugit cum optio numquam nesciunt magnam? Modi,
+            dolorem quisquam?
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
