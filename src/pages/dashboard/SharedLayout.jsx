f2ce35d6f733e2c88555b89f1e3a40298030d75a
Jesse Carson <jmcarson222@gmail.com>
2023-07-14 11:47:32 -0500

    setup dashboard components

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { BigSidebar, SmallSidebar, Navbar } from "../../components";
+import Wrapper from "../../assets/wrappers/SharedLayout";
 
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div>
+            {/* Outlet --> stats, all jobs, add job, profile */}
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
