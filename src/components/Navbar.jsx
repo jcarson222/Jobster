946f2f62244320f1552196c63b50d3d852b94421
Jesse Carson <jmcarson222@gmail.com>
2023-07-14 12:15:17 -0500

    navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,52 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
   return (
     <Wrapper>
-      <div>Navbar</div>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          type="button"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            className="btn"
+            type="button"
+            onClick={() => {
+              console.log("toggle logout dropdown");
+            }}
+          >
+            <FaUserCircle />
+            {user?.name}
+            {/* ^^^ optional chaining since the user value can be null */}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              onClick={() => console.log("logout user")}
+              className="dropdown-btn"
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
