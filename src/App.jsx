7f97dc6885d913899cee636745e5569335825509
Jesse Carson <jmcarson222@gmail.com>
2023-07-17 08:48:52 -0500

    protected route (SharedLayout)

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,29 @@ import {
   Profile,
   SharedLayout,
   Stats,
+  ProtectedRoute,
 } from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        {/* protected SharedLayout */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="/all-jobs" element={<AllJobs />} />
           <Route path="/add-job" element={<AddJob />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
+        {/* end protected SharedLayout */}
+
         <Route path="/landing" element={<Landing />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,5 +3,6 @@ import AllJobs from "./AllJobs";
 import Profile from "./Profile";
 import SharedLayout from "./SharedLayout";
 import Stats from "./Stats";
+import ProtectedRoute from "./ProtectedRoute";
 
-export { AddJob, AllJobs, Profile, SharedLayout, Stats };
+export { AddJob, AllJobs, Profile, SharedLayout, Stats, ProtectedRoute };
