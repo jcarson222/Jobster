9437fb141c1ba19524cb5e4ceaa8aa922bf74304
Jesse Carson <jmcarson222@gmail.com>
2023-07-19 10:04:37 -0500

    get all jobs

--- src/features/allJobs/allJobsSlice.jsx
+++ src/features/allJobs/allJobsSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 import { toast } from "react-toastify";
 
 const initialFiltersState = {
@@ -10,7 +11,7 @@ const initialFiltersState = {
 };
 
 const initialState = {
-  isLoading: true,
+  isLoading: false,
   jobs: [],
   totalJobs: 0,
   numOfPages: 1,
@@ -20,9 +21,45 @@ const initialState = {
   ...initialFiltersState,
 };
 
+const baseURL = "https://jobify-prod.herokuapp.com/api/v1/toolkit";
+
+export const getAllJobs = createAsyncThunk(
+  "allJobs/getJobs",
+  // (_ --> we don't need the first parameter since this is only a 'get' request and we're not sending any data)
+  async (_, thunkAPI) => {
+    const url = "/jobs";
+
+    try {
+      const resp = await axios.get(`${baseURL}${url}`, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllJobs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllJobs.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.jobs = payload.jobs;
+      })
+      .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default allJobsSlice.reducer;
