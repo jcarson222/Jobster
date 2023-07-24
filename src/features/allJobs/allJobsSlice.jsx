7fa36206d05019760e409a35239751f226e7ad8a
Jesse Carson <jmcarson222@gmail.com>
2023-07-24 13:06:31 -0500

    showStats functionality

--- src/features/allJobs/allJobsSlice.jsx
+++ src/features/allJobs/allJobsSlice.jsx
@@ -40,13 +40,11 @@ export const getAllJobs = createAsyncThunk(
 
 export const showStats = createAsyncThunk(
   "allJobs/showStats",
-  // (_ --> we don't need the first parameter since this is only a 'get' request and we're not sending any data)
   async (_, thunkAPI) => {
     const url = "/jobs/stats";
 
     try {
-      const resp = await axios.get(url);
-      console.log(resp.data);
+      const resp = await customFetch.get(url);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -75,6 +73,18 @@ const allJobsSlice = createSlice({
         state.jobs = payload.jobs;
       })
       .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(showStats.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(showStats.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.stats = payload.defaultStats;
+        state.monthlyApplications = payload.monthlyApplications;
+      })
+      .addCase(showStats.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload);
       });
